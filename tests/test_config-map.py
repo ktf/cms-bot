e50a221d20d49fbf421fc4389efa8824b2ae8bf9
Giulio Eulisse <giulio.eulisse@cern.ch>
2014-10-30 22:36:07 +0100

    Have threaded use the CMSSW_7_3_DEVEL_X branch.

--- tests/test_config-map.py
+++ tests/test_config-map.py
@@ -2,7 +2,7 @@
 
 import re
 
-KEYS_RE="(SCRAM_ARCH|RELEASE_QUEUE|PKGTOOLS_TAG|CMSDIST_TAG|RELEASE_BRANCH|ADDITIONAL_TESTS|PR_TESTS|DISABLED|ALWAYS_TAG_CMSSW)"
+KEYS_RE="(SCRAM_ARCH|RELEASE_QUEUE|PKGTOOLS_TAG|CMSDIST_TAG|RELEASE_BRANCH|ADDITIONAL_TESTS|PR_TESTS|DISABLED|ALWAYS_TAG_CMSSW|DO_STATIC_CHECKS)"
 
 if __name__ == "__main__":
   for l in open("config.map").read().split("\n"):
@@ -12,4 +12,5 @@ if __name__ == "__main__":
     for p in l.split(";"):
       assert("=" in p)
       (key, value) = p.split("=")
+      print key
       assert(re.match(KEYS_RE, key))
